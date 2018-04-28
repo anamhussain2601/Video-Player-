e1757c443a95c15e56caf173d7b74245e040705f
Salahuddin Ansari <sansari150894@gmail.com>
2018-04-28 23:06:09 +0530

    code refactoring

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css'
 import Header from './Components/Header';
-import Card from './Components/Card';
+import SearchBar from './Components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './Components/video_list';
+
+const API_KEY = 'AIzaSyBI1qos-cplXHfm_72w9-cPZNARP80e3ew';
 
 class App extends Component {
+
+  constructor(props){
+      super(props);
+      this.state= {videos : []};
+      YTSearch({key: API_KEY , term : 'surfboards'}, (videos) => {
+        // console.log(data);
+        this.setState({videos }  );    //equivalent to    this.setState({videos : videos })
+      });
+    }
+
   render() {
     return (
       <div className="App">
-       <Header></Header>
-     <Card></Card>
+        <SearchBar/>
+        <VideoList videos = {this.state.videos}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -9,41 +9,42 @@ import Header from './Components/Header';
 import YTSearch from 'youtube-api-search';
 import VideoList from './Components/video_list';
 
-const API_KEY = 'AIzaSyBI1qos-cplXHfm_72w9-cPZNARP80e3ew'; 
-
-
-class Appo extends Component{
-    constructor(props){
-        super(props);
-
-        this.state= {videos : []};
-
-
-
-YTSearch({key: API_KEY , term : 'surfboards'}, (videos) => {
-
-    // console.log(data);
-    this.setState({videos }  );    //equivalent to    this.setState({videos : videos })
-
-  }  );
-
-}
-        render(){
-            return(
-            <div>
-                <SearchBar/>
-                {/* <Header/> */}
-                <VideoList videos = {this.state.videos}/>;
-               </div>
-               );
-            
-    }
-}
+// const API_KEY = 'AIzaSyBI1qos-cplXHfm_72w9-cPZNARP80e3ew';
+//
+//
+// class Appo extends Component{
+//     constructor(props){
+//         super(props);
+//
+//         this.state= {videos : []};
+//
+//
+//
+// YTSearch({key: API_KEY , term : 'surfboards'}, (videos) => {
+//
+//     // console.log(data);
+//     this.setState({videos }  );    //equivalent to    this.setState({videos : videos })
+//
+//   }  );
+//
+// }
+//         render(){
+//             return(
+//             <div>
+//                 <SearchBar/>
+//                 {/* <Header/> */}
+//                    <p>Salla</p>
+//                 <VideoList videos = {this.state.videos}/>;
+//                </div>
+//                );
+//
+//     }
+// }
 
 
 // ReactDOM.render(<Header/>, document.getElementById('root'));
 
- ReactDOM.render(<Appo/>, document.getElementById('root'));
+ ReactDOM.render(<App/>, document.getElementById('root'));
 
 
 

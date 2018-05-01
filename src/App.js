dc6fde80a2c27ddc4f910c3aa03ec8532e0a8232
anamhussain <anamhussain2601@gmail.com>
2018-05-01 22:04:52 +0530

    integration with real youtube data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css'
-import Header from './Components/Header';
-import SearchBar from './Components/search_bar';
-import YTSearch from 'youtube-api-search';
-import VideoList from './Components/video_list';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import SearchBar from "./Components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from "./Components/video_list";
+import VideoDetail from "./Components/video_detail";
 
-const API_KEY = 'AIzaSyBI1qos-cplXHfm_72w9-cPZNARP80e3ew';
+const API_KEY = "AIzaSyBI1qos-cplXHfm_72w9-cPZNARP80e3ew";
 
 class App extends Component {
-
-  constructor(props){
-      super(props);
-      this.state= {videos : []};
-      YTSearch({key: API_KEY , term : 'surfboards'}, (videos) => {
-        // console.log(data);
-        this.setState({videos }  );    //equivalent to    this.setState({videos : videos })
-      });
-    }
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: "java" }, videos => {
+      // console.log(data);
+      this.setState({ videos }); //equivalent to    this.setState({videos : videos })
+    });
+  }
 
   render() {
     return (
       <div className="App">
-        <SearchBar/>
-        <VideoList videos = {this.state.videos}/>
+        <SearchBar />
+        <VideoDetail video = {this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
--- src/Components/video_list.js
+++ src/Components/video_list.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import VideoListItem from './Video_List_Item';
+
 
 const VideoList = (props) =>{
+
+    const VideoItem = props.videos.map(( video) => { 
+
+        return <VideoListItem key = {video.etag} video = {video}/>
+    } );
     return(
         <ul className = "col-md-4 list-group">
-        {props.videos.length}
+        {/* {props.videos.length} */}
+        {VideoItem}
         </ul>
     );
 };
